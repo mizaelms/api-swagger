d05106c4aece07c9c7000a0138e7afa5e5f72bca
Mizael <mizaelm@hotmail.com>
2019-10-04 17:42:00 -0500

    fonts-cloud

---
+++ bin/js/loader.js
@@ -0,0 +1,24 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+$(document).ready(function() {
+	var url = getUrlParameter('url');
+	console.log(url);
+	$( "#api" ).load( url , function() {
+		  alert( "Load was performed." );
+	});
+	
+    
+} );
